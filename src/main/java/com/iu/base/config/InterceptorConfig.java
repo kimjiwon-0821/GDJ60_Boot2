77d1fac1f10a8480aaf44eb2ffcf7929bacf11aa
kimjiwon-0821 <wldnjsqkek29@naver.com>
2023-04-21 15:26:01 +0900

    Interceptors-pre

---
+++ src/main/java/com/iu/base/config/InterceptorConfig.java
@@ -0,0 +1,38 @@
+package com.iu.base.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.iu.base.interceptors.AdminCheckInterceptor;
+import com.iu.base.interceptors.MemberCheckInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private MemberCheckInterceptor membercheckInterceptor;
+	@Autowired
+	private AdminCheckInterceptor adminCheckInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		
+		registry.addInterceptor(membercheckInterceptor)
+					.addPathPatterns("/member/mypage")
+					.addPathPatterns("/qna/*")
+					.excludePathPatterns("/qna/list")
+					.addPathPatterns("/member/admin")
+					.addPathPatterns("/notice/*")
+					.excludePathPatterns("/notice/list")
+					.excludePathPatterns("/notice/detail");
+					
+		registry.addInterceptor(adminCheckInterceptor)
+					.addPathPatterns("/member/admin")
+					.addPathPatterns("/notice/*")
+					.excludePathPatterns("/notice/list")
+					.excludePathPatterns("/notice/detail");
+	}
+}
